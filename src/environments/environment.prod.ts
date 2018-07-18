104ac3c34db4b4ab9d3bc8904addd9892080b596
Amabel <luoweibinb@gmail.com>
2018-07-18 11:42:31 +0900

    feat(oauth): add github oauth request

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   domainName: 'gitpulse.net',
-  apiEndpoint: 'https://api.github.com'
+  apiEndpoint: 'https://api.github.com',
+  githubClientId: 'ad277de13708451b031c',
+  githubClientSecret: 'baee95eb43d9e710498bb9dd3955f9c71fc59228'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,10 @@
 export const environment = {
   production: false,
   domainName: 'localhost:4200',
-  apiEndpoint: 'https://api.github.com'
+  apiEndpoint: 'https://api.github.com',
+  githubClientId: 'ad277de13708451b031c',
+  githubClientSecret: 'baee95eb43d9e710498bb9dd3955f9c71fc59228'
+
 };
 
 /*
