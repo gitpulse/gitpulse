d6592ccb7d5cbdf13dd0f184ec6d4e7b625bd395
Amabel <luoweibinb@gmail.com>
2018-07-18 11:04:48 +0900

    feat(base.service): add get and post requests

---
+++ src/app/shared/lib/services/base.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BaseService {
+  private headers = new HttpHeaders( {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  });
+
+  constructor(
+    protected http: HttpClient
+  ) { }
+
+  getRequest(url: string, params = {}): Observable<any> {
+    return this.http.get(url, { params }).pipe(
+      catchError((error: HttpErrorResponse) =>  {
+        console.log(error);
+        return of(error);
+      })
+    );
+  }
+
+  postRequest(url: string, body: object = null): Observable<any> {
+    const requestBody = body;
+    return this.http.post(url, requestBody, { headers: this.headers }).pipe(
+      catchError((error: HttpErrorResponse) =>  {
+        console.log(error);
+        return of(error);
+      })
+    );
+  }
+
+}
